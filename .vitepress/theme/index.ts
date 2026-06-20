22f89122a34a35ec1a9ede09c9273d3c7cb0ed32
axibayuit <axibayuit@gmail.com>
2026-06-21 00:00:40 +0800

    docs: 优化移动端表格排版

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,6 +1,7 @@
 import DefaultTheme from 'vitepress/theme'
 import type { Theme } from 'vitepress'
 import RedirectHome from './RedirectHome.vue'
+import './style.css'
 
 const docsPathToLocale: Record<string, string> = {
   cn: 'zh-CN',
